58fcbc6d420c7c89fbdf8697ff3598f64bcb71bc
mohamedrashadrouine <mohamedrashadrouine.com>
2024-12-18 22:42:47 +0100

    "Updated Chatbot options for email, WhatsApp, and office location"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Hero from '../components/home/Hero';
 import FeaturedProperties from '../components/home/FeaturedProperties';
 import ServiceOverview from '../components/home/ServiceOverview';
-import './Home.css'; // Importing CSS for background effect
 
 const Home = () => {
   return (
-    <div className="home-background">
+    <div>
       <Hero />
       <ServiceOverview />
       <FeaturedProperties />
