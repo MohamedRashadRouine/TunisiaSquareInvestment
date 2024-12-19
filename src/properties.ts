03b328eb7ed52473f68d344defee37631eac0684
mohamedrashadrouine <mohamedrashadrouine.com>
2024-12-19 22:02:49 +0100

    raw img oops fix

--- src/properties.ts
+++ src/properties.ts
@@ -22,7 +22,8 @@ export const properties = [
     title: 'Villa de luxe à Chotrana 3',
     location: 'Chotrana 3',
     price: '1,250,000 TND',
-    image: 'https://raw.githubusercontent.com/MohamedRashadRouine/TunisiaSquareInvestment/main/src/assets/images/properties/villa-de-luxe-a-chotrana-3/face-view.jpg'    features: ['S+6', 'Piscine', '700 m²', '835 m² de terrain', 'Chauffage central', 'Climatisation'],
+    image: "https://raw.githubusercontent.com/MohamedRashadRouine/TunisiaSquareInvestment/main/src/assets/images/properties/villa-de-luxe-a-chotrana-3/face-view.jpg",
+    features: ['S+6', 'Piscine', '700 m²', '835 m² de terrain', 'Chauffage central', 'Climatisation'],
     facebookPostUrl: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FTunisiaSquareInvestment%2Fposts%2Fpfbid033FsikMnY6eSQ7Y5rVc2bETW6kq2BSzstNfN7gHQhHoRJasscS4KrE5EcKjoxNCvtl&show_text=true&width=500"
   }
 ];
