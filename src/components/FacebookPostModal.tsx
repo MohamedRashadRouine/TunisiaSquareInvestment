4e71d5a9d9f2e4738c3f4c8971448954f258022f
mohamedrashadrouine <mohamedrashadrouine.com>
2024-12-19 22:00:46 +0100

    raw img link, and fb modal adjustment

--- src/components/FacebookPostModal.tsx
+++ src/components/FacebookPostModal.tsx
@@ -10,7 +10,7 @@ const FacebookPostModal: React.FC<FacebookPostModalProps> = ({ isOpen, onClose, 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center" onClick={onClose}>
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-60" onClick={onClose}>
       <div className="relative" onClick={(e) => e.stopPropagation()}> 
         <button className="absolute top-0 right-0 m-2 text-white" onClick={onClose}>X</button>
         <iframe 
