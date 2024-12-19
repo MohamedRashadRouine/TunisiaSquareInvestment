6075e69c94b35b1da9b9dba3037d4f8de2ba8d21
mohamedrashadrouine <mohamedrashadrouine.com>
2024-12-19 23:35:32 +0100

    fb page placeholders

--- src/properties.ts
+++ src/properties.ts
@@ -1,29 +1,41 @@
-export const properties = [
-  {
-    id: 1,
-    title: 'Villa Moderne à La Marsa',
-    location: 'La Marsa',
-    price: '1,200,000 TND',
-    image: 'https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    features: ['5 Chambres', '4 Salles de bain', 'Piscine', 'Jardin'],
-    facebookPostUrl: "https://www.facebook.com/plugins/post.php?href=YOUR_POST_URL_1"
-  },
-  {
-    id: 2,
-    title: 'Appartement Modern aux Berges du Lac',
-    location: 'Les Berges du Lac',
-    price: '950,000 TND',
-    image: 'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    features: ['3 Chambres', '2 Salles de bain', 'Vue sur lac', 'Parking'],
-    facebookPostUrl: "https://www.facebook.com/plugins/post.php?href=YOUR_POST_URL_2"
-  },
-  {
-    id: 3,
-    title: 'Villa de luxe à Chotrana 3',
-    location: 'Chotrana 3',
-    price: '1,250,000 TND',
-    image: "https://raw.githubusercontent.com/MohamedRashadRouine/TunisiaSquareInvestment/main/src/assets/images/properties/villa-de-luxe-a-chotrana-3/face-view.jpg",
-    features: ['S+6', 'Piscine', '700 m²', '835 m² de terrain', 'Chauffage central', 'Climatisation'],
-    facebookPostUrl: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FTunisiaSquareInvestment%2Fposts%2Fpfbid033FsikMnY6eSQ7Y5rVc2bETW6kq2BSzstNfN7gHQhHoRJasscS4KrE5EcKjoxNCvtl&show_text=true&width=500"
-  }
+export const properties: Property[] = [
+    {
+        id: 1,
+        title: 'Villa Moderne à La Marsa',
+        location: 'La Marsa',
+        price: '1,200,000',
+        priceValue: 1200000,
+        image: 'https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        features: ['5 Chambres', '4 Salles de bain', 'Piscine', 'Jardin'],
+        facebookPostUrl: "https://www.facebook.com/TunisiaSquareInvestment",
+        beds: 5,
+        baths: 4,
+        area: 300
+    },
+    {
+        id: 2,
+        title: 'Appartement Modern aux Berges du Lac',
+        location: 'Les Berges du Lac',
+        price: '950,000',
+        priceValue: 950000,
+        image: 'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        features: ['3 Chambres', '2 Salles de bain', 'Vue sur lac', 'Parking'],
+        facebookPostUrl: "https://www.facebook.com/TunisiaSquareInvestment",
+        beds: 3,
+        baths: 2,
+        area: 150
+    },
+    {
+        id: 3,
+        title: 'Villa de luxe à Chotrana 3',
+        location: 'Chotrana 3',
+        price: '1,250,000',
+        priceValue: 1250000,
+        image: "https://raw.githubusercontent.com/MohamedRashadRouine/TunisiaSquareInvestment/main/src/assets/images/properties/villa-de-luxe-a-chotrana-3/face-view.jpg",
+        features: ['S+6', 'Piscine', '700 m²', '835 m² de terrain', 'Chauffage central', 'Climatisation'],
+        facebookPostUrl: "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FTunisiaSquareInvestment%2Fposts%2Fpfbid033FsikMnY6eSQ7Y5rVc2bETW6kq2BSzstNfN7gHQhHoRJasscS4KrE5EcKjoxNCvtl&show_text=true&width=500",
+        beds: 6,
+        baths: 4,
+        area: 700
+    },
 ];
